4a8eb824d905bbc6169c8579a40c3155276386df
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-10-01 00:00:44 +0000

    Use setters to initialize personType member attributes

--- L10_A.cpp
+++ L10_A.cpp
@@ -108,50 +108,55 @@ int main() {
 
     // Init members
 
-    personType p1{
-        "Sabrina",
-        "Morales",
-        "662 S. Southhampton Avenue Vista, CA 92083",
-        51.2,
-        "24 July 2017",
-        'F'
-    };
+    personType p1{};
 
-    personType p2{
-        "Elle",
-        "Macdonald",
-        "84 Green Drive Tracy, CA 95376",
-        52.3,
-        "17 September 2014",
-        'F'
-    };
+    p1.setFirstName("Sabrina");
+    p1.setLastName("Morales");
+    p1.setAddress("662 S. Southhampton Avenue Vista, CA 92083");
+    p1.setHeight(51.2);
+    p1.setDOB("24 July 2017");
+    p1.setGender('F');
+    p1.setAge(20);
 
-    personType p3{
-        "Hannah",
-        "Finley",
-        "8253 Linda Dr. Laguna Niguel, CA 92677",
-        49.8,
-        "13 September 2012",
-        'F'
-    };
+    personType p2{};
 
-    personType p4{
-        "Arabella",
-        "Walter",
-        "7593 Wood St. Fountain Valley, CA 92708",
-        54.1,
-        "27 April 2010",
-        'F'
-    };
+    p2.setFirstName("Elle");
+    p2.setLastName("Macdonald");
+    p2.setAddress("84 Green Drive Tracy, CA 95376");
+    p2.setHeight(52.3);
+    p2.setDOB("17 September 2014");
+    p2.setGender('F');
+    p2.setAge(20);
 
-    personType p5{
-        "Kye",
-        "Baird",
-        "8316 Lafayette Ave. North Hills, CA 91343",
-        55.8,
-        "28 November 2008",
-        'F'
-    };
+    personType p3{};
+
+    p3.setFirstName("Hannah");
+    p3.setLastName("Finley");
+    p3.setAddress("8253 Linda Dr. Laguna Niguel, CA 92677");
+    p3.setHeight(49.8);
+    p3.setDOB("13 September 2012");
+    p3.setGender('F');
+    p3.setAge(20);
+
+    personType p4{};
+
+    p4.setFirstName("Arabella");
+    p4.setLastName("Walter");
+    p4.setAddress("7593 Wood St. Fountain Valley, CA 92708");
+    p4.setHeight(54.1);
+    p4.setDOB("27 April 2010");
+    p4.setGender('F');
+    p4.setAge(20);
+
+    personType p5{};
+
+    p5.setFirstName("Kye");
+    p5.setLastName("Baird");
+    p5.setAddress("8316 Lafayette Ave. North Hills, CA 91343");
+    p5.setHeight(55.8);
+    p5.setDOB("28 November 2008");
+    p5.setGender('F');
+    p5.setAge(20);
 
     // Stretch 1 (Heading)
     cout << left << setw(10) << setfill('-') << "" << " # personType class # " << left << setw(10) << setfill('-') << "" << endl;
